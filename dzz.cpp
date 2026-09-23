9b8e2e26a76494f67fe2049a942ea7355ec1f2c3
Semion <alexurupin91@gmail.com>
2026-09-23 15:36:51 +0900

    add files

---
+++ dzz.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    cout << "#task1\n";
+    cout << "Name       : \"The war and the peace\"\n" "Avtor      : L.N Tolstoj\n"
+        "Izdatelstvo: Piter\n" "Pages      : 500.\n";
+
+    cout << "#task2\n";
+    cout << "   \"U lukomor’a dub zelenij,\a\a\a\n";
+    cout << "   Zlataya zep na dupe tom,\a\a\a\n";
+    cout << "   I dnem i nochju kot uchenij\a\a\a\n";
+    cout << "   vse hodit po cepi krugom\a\a\a\"";
+
+    cout << "\n" << (char)201 << string(45, (char)205) << (char)187;
+    cout << " \n";
+    cout << "\n" << (char)186 << string(10, ' ') << "Vremena goda" << string(23, ' ') << (char)186;
+    cout << " \n";
+    cout << "\n" << (char)204 << string(10, (char)205) << (char)203 << string(11, (char)205) << (char)203 << string(10, (char)205) << (char)203 << string(11, (char)205) << (char)185;
+    cout << "\n";
+    cout << "\n" << (char)186 << "   Zima   " << (char)186 << "   Vesna   " << (char)186 << "   Leto   " << (char)186 << "   Osen    " << (char)186;
+    cout << "\n";
+    cout << "\n" << (char)200 << string(10, (char)205) << (char)202 << string(11, (char)205) << (char)202 << string(10, (char)205) << (char)202 << string(11, (char)205) << (char)188;
+}
